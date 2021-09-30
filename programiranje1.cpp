977edb8e8ef84c23cf7888565dbd7e68fb4cc0e6
AdnanR22 <91673407+AdnanR22@users.noreply.github.com>
2021-09-30 18:49:15 +0200

    Add files via upload

--- programiranje1.cpp
+++ programiranje1.cpp
@@ -3,13 +3,14 @@ using namespace std;
 int main ()
 {
 int a;
-if(a% 3==0 && a% 5==0)
+cin>>a;
+if(a%3==0 && a%5==0)
 cout<<"Broj je djeljiv i sa 3 i sa 5 "<<endl;
-if(a% 3==0 a% 5!=0)
+else if(a%3==0 && a%5!=0)
 cout<<"Broj je djeljiv sa 3 ali nije djeljiv sa 5 "<<endl;
-if(a% 5==0 a% 3!=0)
+else if(a%5==0 && a%3!=0)
 cout<<"Broj je djeljiv sa 5 ali nije sa 3 "<<endl;
-if(a% 5!=0 a% 3!=0)
+else if(a%5!=0 && a%3!=0)
 cout<<"Broj nije djeljiv ni sa 5 ni sa 3"<<endl;
 return 0;
 }
